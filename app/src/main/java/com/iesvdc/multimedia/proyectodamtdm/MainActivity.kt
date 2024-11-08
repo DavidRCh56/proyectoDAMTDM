4a920c7ec35184d39e611fa31625fcd532523386
DavidRCh56 <davricchi@gmail.com>
2024-11-08 13:12:20 +0100

    login mas vistoso y añadido enableEdgeToEdge()

--- app/src/main/java/com/iesvdc/multimedia/proyectodamtdm/MainActivity.kt
+++ app/src/main/java/com/iesvdc/multimedia/proyectodamtdm/MainActivity.kt
@@ -17,8 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        enableEdgeToEdge()
-
         val rvRecipes = findViewById<RecyclerView>(R.id.rvRecipes)
         rvRecipes.layoutManager = LinearLayoutManager(this)
 
